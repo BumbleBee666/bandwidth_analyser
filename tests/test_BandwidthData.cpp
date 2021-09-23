83791ffac1ad4f9ca18631f93951a2c1120b045a
BumbleBee666 <73425164+BumbleBee666@users.noreply.github.com>
2021-09-23 15:40:37 +0100

    Add files via upload

--- tests/test_BandwidthData.cpp
+++ tests/test_BandwidthData.cpp
@@ -2,14 +2,27 @@
 #include "gtest/gtest.h"
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
+#include <string>
+#include <memory>
  
-TEST(BandwidthDataTest, Negative) {
-  BandwidthData a("..//tests//testdata//");
-  std::this_thread::sleep_for(std::chrono::seconds(20));
-  EXPECT_EQ(a.GetNoOfDays(), 92);
+const std::string testdata = "..//tests//testdata//";
+
+std::unique_ptr<BandwidthData> bandwidthData;
+
+TEST(BandwidthDataTest, NumberOfDays) {
+  EXPECT_EQ(bandwidthData->GetNoOfDays(), 92);
+}
+
+TEST(BandwidthDataTest, DayDate) {
+  BandwidthDay day = bandwidthData->GetDay("20210601");
+  EXPECT_EQ(day.Date(), "20210601");
 }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+
+  bandwidthData = std::make_unique<BandwidthData>(testdata);
+  std::this_thread::sleep_for(std::chrono::seconds(30));
+
   return RUN_ALL_TESTS();
 }
