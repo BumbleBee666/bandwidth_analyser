8f77a5dec2219e6aef60981b6a2571dc4eb0494c
BumbleBee666 <73425164+BumbleBee666@users.noreply.github.com>
2021-09-24 16:53:08 +0100

    Add files via upload

--- tests/test_BandwidthData.cpp
+++ tests/test_BandwidthData.cpp
@@ -1,9 +1,11 @@
 #include "BandwidthData.h"
+#include "BandwidthStatistics.h"
 #include "gtest/gtest.h"
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
 #include <string>
 #include <memory>
+#include <iostream>
  
 const std::string testdata = "..//tests//testdata//";
 
@@ -16,6 +18,26 @@ TEST(BandwidthDataTest, NumberOfDays) {
 TEST(BandwidthDataTest, DayDate) {
   BandwidthDay day = bandwidthData->GetDay("20210601");
   EXPECT_EQ(day.Date(), "20210601");
+}
+
+TEST(BandwidthDataTest, DayDataPointCount) {
+  BandwidthDay day = bandwidthData->GetDay("20210601");
+  const std::map<std::string, const std::unique_ptr<const BandwidthDataPoint>>& dataPoints = day.DataPoints();
+  EXPECT_EQ(dataPoints.size(), 288);
+}
+
+TEST(BandwidthDataTest, AverageStatistic) {
+  auto statistics = bandwidthData->GetStatistics("202106");
+  if (statistics != NULL)
+  {
+    std::string requiredTime = "0000";
+    double average = (*statistics)[requiredTime]->Average();
+    double low = (*statistics)[requiredTime]->Low();
+    double high = (*statistics)[requiredTime]->High();
+    EXPECT_EQ(std::to_string(average), "55.459115");
+    EXPECT_EQ(std::to_string(low), "7.649696");
+    EXPECT_EQ(std::to_string(high), "67.493416");
+  }
 }
 
 int main(int argc, char **argv) {
