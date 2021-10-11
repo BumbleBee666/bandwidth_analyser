f0b3aee9874ffcade29550d1e23cabbd79796984
BumbleBee666 <73425164+BumbleBee666@users.noreply.github.com>
2021-10-11 15:00:55 +0100

    Add files via upload
    Removed virtual declaration from non-virtual methods.

--- JSONBase.h
+++ JSONBase.h
@@ -14,8 +14,8 @@ public:
     bool DeserializeFromFile(const std::string& filePath);
     bool SerializeToFile(const std::string& filePath); 	
 
-    virtual std::string Serialize() const;
-    virtual bool Deserialize(const std::string& s);
+    std::string Serialize() const;
+    bool Deserialize(const std::string& s);
     virtual bool Deserialize(const rapidjson::Value& obj) = 0;
     virtual bool Serialize(rapidjson::Writer<rapidjson::StringBuffer>* writer) const = 0;
 protected:	
